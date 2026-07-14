9d3b94b074b80e898498bb9906939a14f38dc584
Light Play <lightplay456@gmail.com>
2026-07-14 17:33:04 +0300

    fix: fixed a typo after Srp Group.Rfc5054_1536; there was no semicolon.

--- src/srp/srp-group-params.ts
+++ src/srp/srp-group-params.ts
@@ -27,7 +27,7 @@ export class SrpGroupParams {
                     "E3BAB63D" + "47548381" + "DBC5B1FC" + "764E3F4B" + "53DD9DA1" + "158BFD3E" + "2B9C8CF5" +
                     "6EDF0195" + "39349627" + "DB2FD53D" + "24B7C486" + "65772E43" + "7D6C7F8C" + "E442734A" +
                     "F7CCB7AE" + "837C264A" + "E3A9BEB8" + "7F8A2FE9" + "B8B5292E" + "5A021FFF" + "5E91479E" +
-                    "8CE7A28C" + "2442C6F3" + "15180F93" + "499A234D" + "CF76E3FE" + "D135F9BB")
+                    "8CE7A28C" + "2442C6F3" + "15180F93" + "499A234D" + "CF76E3FE" + "D135F9BB");
             case SrpGroup.Rfc5054_2048:
                 return BigInt("0x" +
                     "AC6BDB41" + "324A9A9B" + "F166DE5E" + "1389582F" + "AF72B665" + "1987EE07" + "FC319294" +
